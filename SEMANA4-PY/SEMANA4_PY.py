035f2325713bb3e01f4bee38d5dd2dcfec8035d8
choiisita <aylingutierrez1806@gmail.com>
2025-09-16 15:56:50 -0500

    ejer 2 sem 5

--- SEMANA4-PY/SEMANA4_PY.py
+++ SEMANA4-PY/SEMANA4_PY.py
@@ -9,4 +9,59 @@ def ejer1():
         else:
             print("Adulto mayor")
 
+def ejer2():
+    annio = int(input("Ingrese el año: "))
+
+    if (annio %4 ==0 and annio %100 !=0) or (annio %400 =0):
+        print("El año es bisisesto")
+
+    else:
+        print("\nEl año es bisiesto")
+    
+    if annio %2 ==0:
+        print("El año es par")
+    else:
+        print("El año es impar")
+
+def ejer3():
+    monto = float(input("Ingrese el monto en soles: "))
+
+    print("\n1. Dolares\n2. Euros")
+
+    opcion=int(input("\nIngrese una opcion: "))
+
+    match opcion:
+        case 1: print("Dolares: ", round((monto/3.75),0))
+        case 2: print("Euros: {monto/4.05:.2f}")
+        case 3: print("Opcion incorrecta")
+
+import math
+
+def ejer4():
+    print("Bienvenido al sistema de calculos de areas")
+    print("1. Cuadrado")
+    print("2. Rectangulo")
+    print("3. Triangulo")
+    print("4. Circulo\n")
+
+    opcion = int(input("Ingrese una opcion: "))
+
+    match opcion:
+        case 1:
+            lado = int(input("Ingrese lado: "))
+            print("Area: ",lado*lado)
+        case 2:
+            bse = int(input("Ingrese la base: "))
+            alt = int(input("Ingrese la altura: "))
+            print("Area rectangulo: ";(bse*alt))
+        case 3:
+            base2 = int(input("Ingrese la base: "))
+            alt2 = int(input("Ingrese la altura: "))
+            print("Area triangulo: ",(bse*alt2)/2)
+        case 4:
+            radio = float(input("Ingrese el radio: "))
+            print("Area del circulo: ",(round(math.pi * radio**2),2))
+
+        case 5:
+
 
---
+++ SEMANA5.PY/SEMANA5.PY.py
@@ -0,0 +1,28 @@
+def ejer1()
+edad = int(input("Ingrese su edad: "))
+
+if edad >= 18
+    print("Puede votar.")
+
+    if edad >=25
+        print("Candidato paras un cargo politico")
+
+    else: 
+        print("No es candidato para un cargo politico")
+
+else:
+    print("No puede votar.")
+    print("No puede ejercer un cargo politico.")
+
+ejer1()
+
+def ejer2():
+    lado1 = int(input("Ingrese lado1: "))
+    lado2 = int(input("Ingrese lado2: "))
+    lado3 = int(input("Ingrese lado3: "))
+
+    if(lado1 == lado 2 == lado 3):
+        print("EQUILATERO")
+    elif lado1 == lado2 or lado1 == lado3 or lado2 == lado3:
+        print("ISOSCELES")
+    else:
